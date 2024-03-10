468f4b4286776981eb67fc1f452d4fc6a27e0a12
NotSleepp <danielfranconot@gmail.com>
2024-03-10 01:05:57 -0300

    terminado

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    {id : 'ARS', nombre : 'Peso Argentino papá'},
+    {id : 'USD', nombre : 'Dolar Estadounidense'},
+    {id : 'MXN', nombre : 'Peso Mexicano'},
+    {id : 'EUR', nombre : 'Euro'},
+    {id : 'GBP', nombre : 'Libra Esterlina'},
+]
+
+export {
+    monedas
+}
